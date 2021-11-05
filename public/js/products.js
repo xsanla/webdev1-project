91b05b1376a93ad7eb5fd5247ee677c08964118d
Mikko Nurminen <mikko.nurminen@tut.fi>
2021-11-05 11:46:24 +0200

    Ninth exercise round exercises added

---
+++ public/js/products.js
@@ -0,0 +1,17 @@
+const addToCart = (productId, productName) => {
+  // TODO 9.2
+  // use addProductToCart(), available already from /public/js/utils.js
+  // /public/js/utils.js also includes createNotification() function
+};
+
+(async() => {
+  //TODO 9.2 
+  // - get the 'products-container' element
+  // - get the 'product-template' element
+  // - use getJSON(url) to get the available products
+  // - for each of the products:
+  //    * clone the template
+  //    * add product information to the template clone
+  //    * remember to add an event listener for the button's 'click' event, and call addToCart() in the event listener's callback
+  // - remember to add the products to the the page
+})();

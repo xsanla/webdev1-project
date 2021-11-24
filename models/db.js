efddfe0134b721808fe622ef3bec36c5ef2690f3
Nadezda Belonogova <nadezda.belonogova@tuni.fi>
2021-11-24 15:07:23 +0200

    implement getDbUrl

--- models/db.js
+++ models/db.js
@@ -10,7 +10,8 @@ const mongoose = require('mongoose');
  */
 const getDbUrl = () => {
   // TODO: 9.4 Implement this
-  require('dotenv').config({ path: `/home/antti/webdev1/webdev1-group24/.env` });
+ 
+  require('dotenv').config(); 
   return process.env.DBURL;
 };
 
